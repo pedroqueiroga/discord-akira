eab6c7174c294d527f1022575b987fee348434a9
Pedro Queiroga <psq@cin.ufpe.br>
2020-04-19 16:43:23 -0300

    Skeleton of command: toca

--- src/commands.py
+++ src/commands.py
@@ -3,6 +3,17 @@ from discord.ext.commands import command
 
 @command()
 async def echo(ctx, *args):
-    """ecoa os argumentos"""
+    """
+    Ecoa os argumentos
+    """
     response = ' '.join(args)
     await ctx.send(response)
+
+
+@command()
+async def toca(ctx, *, args):
+    """
+    Toca a música no canal de voz do invocador. Continua tocando no canal de voz
+    em que estiver.
+    """
+    
