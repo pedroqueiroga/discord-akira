e3d8a28ce940c69ed09d28d721c68fa5902a4c62
Pedro Queiroga <psq@cin.ufpe.br>
2021-05-29 13:06:16 -0300

    Creates `deejay.Guild` class
    This class will be instantiated once per guild that Akira is commanded upon

---
+++ src/cogs/deejay/guild.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from .song import Song
+
+
+class Guild:
+    _setlist: List[Song] = []
+    _current_song: Song
+    _stopped_playing_timestamp: float
+    _loudness: float
+
+    def __init__(self, loudness: float = 1):
+        self._loudness = loudness
+
+    @property
+    def setlist(self):
+        return self._setlist
+
+    @property
+    def current_song(self):
+        return self._current_song
+
+    @current_song.setter
+    def current_song(self, current_song: Song):
+        self._current_song = current_song
+
+    @property
+    def stopped_playing_timestamp(self):
+        return self._stopped_playing_timestamp
+
+    @stopped_playing_timestamp.setter
+    def stopped_playing_timestamp(self, timestamp: float):
+        self._stopped_playing_timestamp = timestamp
+
+    @property
+    def loudness(self):
+        return self._loudness
+
+    @loudness.setter
+    def loudness(self, loudness: float):
+        self._loudness = loudness
